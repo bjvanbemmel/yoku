3dd364ceb2b66ad440757297a121923e55e50b07
bjvanbemmel <97085143@st.deltion.nl>
2023-02-05 14:12:18 +0100

    Bad mistake. Reverted the revert

--- cmd/main.go
+++ cmd/main.go
@@ -36,7 +36,7 @@ func registerVisit(c *router.Context) {
 	json.NewDecoder(c.Request.Body).Decode(&vb)
 
 	var ip string
-	ip = c.Request.Header.Get("X-Forwarded-For")
+	ip = c.Request.Header.Get("X-Real-IP")
 
 	if ip == "" {
 		var err error
